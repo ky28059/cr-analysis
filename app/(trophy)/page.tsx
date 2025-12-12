4bbb6aa8edb1bab7898b231540a96c50ce63cd90
Kevin Yu <kevinyu567@gmail.com>
2025-12-12 14:21:23 -0500

    Fix `trail` being counted as trophy road

--- app/(trophy)/page.tsx
+++ app/(trophy)/page.tsx
@@ -4,7 +4,7 @@ import TrophyAnalysis from '@/app/(trophy)/TrophyAnalysis';
 
 export default async function TrophyRoad() {
     const battles = await getCachedBattles();
-    const filtered = battles.filter((b) => b.type === 'PvP' || b.type === 'trail');
+    const filtered = battles.filter((b) => b.type === 'PvP' || (b.type === 'trail' && b.gameMode.name === 'Ladder'));
 
     return <TrophyAnalysis battles={filtered} />;
 }
